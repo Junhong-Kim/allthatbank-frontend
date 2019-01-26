10e9ce0a82769e4c4d4f2d822e72083a2099bd79
Junhong-Kim <jhk1005jhk@gmail.com>
2019-01-26 19:59:55 +0900

    Add: Vuetify, Material Icons

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-toolbar color="primary" dark fixed app>
+      <v-toolbar-title>올댓뱅크 BETA</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex text-xs-center></v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="primary" app>
+      <span class="white--text">&copy; Junhong-Kim</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+
+Vue.config.productionTip = false;
+Vue.use(Vuetify, {
+  theme: {
+    primary: "#2196f3",
+    secondary: "#6ec5ff",
+    accent: "#0068bf",
+    error: "#ff6090"
+  },
+  iconfont: "md"
+});
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
