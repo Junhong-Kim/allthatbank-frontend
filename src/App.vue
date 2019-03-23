cc95b58cf4c78e957b88559beabac1f17d2d0665
Junhong-Kim <jhk1005jhk@gmail.com>
2019-03-24 01:00:53 +0900

    Modify: footer

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,23 @@
     <v-content>
       <router-view></router-view>
     </v-content>
-    <v-footer color="primary" app>
-      <span class="white--text">&copy; Junhong-Kim</span>
+    <v-footer dark height="auto">
+      <v-layout column wrap primary pa-2 class="text-xs-center">
+        <v-flex style="font-size: 8px;">
+          <div>
+            올댓뱅크는 "금융상품통합비교공시시스템(금융감독원) 금융회사 상품정보" 오픈 API 서비스를 사용한 결과입니다.
+            올댓뱅크에서 제공되는 금융회사 상품정보를 복제 또는 저장하거나 전송할 수 없습니다.
+          </div>
+          <div>
+            올댓뱅크에서 제공되는 금융회사 상품정보는 참고사항이며 오류가 발생하거나 지연될 수 있습니다.
+            올댓뱅크에서 제공되는 금융회사 상품정보에 대한 법적인 책임을 지지 않습니다.
+          </div>
+        </v-flex>
+        <v-divider class="ma-2"></v-divider>
+        <v-flex class="caption">
+          <span>Made By. <strong>Junhong Kim</strong></span>
+        </v-flex>
+      </v-layout>
     </v-footer>
   </v-app>
 </template>
